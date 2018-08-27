d7682d689fa937084a3a2fcde186230f10706f5e
manshu12 <anshu1.manshu2@gmail.com>
2018-08-27 18:21:24 +0530

    Assignment completed

--- valid.php
+++ valid.php
@@ -1,56 +1,17 @@
-<?php 
-
-$error = "";
-$username = "";
-$pass = "";
-
-
-function clean_txt($string)
-{
-    $string = trim($string);
-    $string = stripslashes($string);
-    $string = htmlspecialchars($string);
-    return $string;
+<?php
+$username=$_POST['username'];
+if(isset($_POST['username']) && isset($_POST['pass'])) {
+    $data = $_POST['username'] . '-' . $_POST['pass'] . "\r\n";
+    $ret = file_put_contents('mytext.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+         $myfile = fopen("mytext.txt", "r") or die("Unable to open file!");
+         echo 'welcome ' . fread($myfile,strlen($username)). ' again';
+         fclose($myfile);
+    }
 }
-if(isset($_POST["submit"]))
-{
-    if(empty($_POST["usename"]))
-    {
-        $error.= '<p><label class="text-danger">Please Enter Your Name.<label/></p>';
-    }
-    else
-    {
-        $username = clean_text($_POST["username"]);
-    }
-    
-    if(empty($_POST["pass"]))
-    {
-        $error.= '<p><label class="text-danger">Please Enter Your Password.<label/></p>';
-    }
-    else
-    {
-        $pass = clean_text($_POST["pass"]);
-    }
-    
-    if($error == "")
-    {
-        $file_open = fopen("logindata.csv", "a");
-        $no_row = count(file("logindata.csv"));
-        if($no_row > 1)
-        {
-            $no_row = ($no_row -1) +1;
-        }
-        $form_data = array(
-            'sr no'    => $no_row,
-            'username' => $username,
-            'password' => $pass
-        );
-        fputcsv($file_open,$form_data);
-        $username = "";
-        $password = "";
-         
-    }
-    echo $error;
+else {
+   die('no post data to process');
 }
-
-?>
